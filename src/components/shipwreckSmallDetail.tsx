53e5206e0e9f06e4d9204380c6030512bca85086
Kieran Parikh <kieran@parikh.ca>
2024-10-02 14:11:19 -0400

    side view

--- src/components/shipwreckSmallDetail.tsx
+++ src/components/shipwreckSmallDetail.tsx
@@ -15,40 +15,40 @@ const ShipwreckSmallDetail: React.FC<ShipwreckSmallDetailProps> = (props) => {
         }
         return (
             <div style={{overflowY: "auto", maxHeight: "100%"}}>
-                    <h5 style={{color: "red", cursor: "pointer"}} onClick={() => {props.setSelectedShip(null)}}> Clear selection</h5>
+                <div style={{ overflowY: "auto", maxHeight: "100%" }}>
+                    <h5 style={{ color: "red", cursor: "pointer" }} onClick={() => { props.setSelectedShip(null) }}> Clear selection</h5>
                     <h1>{props.shipwreck.Ship}</h1>
-                    <h3>id</h3>
-                    <p>{props.shipwreck.id}</p>
-                    <h3>imgPath</h3>
-                    <p>{props.shipwreck.imgPath}</p>
-                    <h3>Dataset</h3>
-                    <p>{props.shipwreck.Dataset}</p>
-                    <h3>Depth</h3>
-                    <p>{props.shipwreck.Depth}</p>
-                    <h3>ImgPath\_\_3d</h3>
-                    <p>{props.shipwreck.ImgPath__3d}</p>
-                    <h3>Destination\_Port</h3>
-                    <p>{props.shipwreck.Destination_Port}</p>
-                    <h3>Lake</h3>
-                    <p>{props.shipwreck.Lake}</p>
-                    <h3>ImgPath\_\_Historical\_other</h3>
-                    <p>{props.shipwreck.ImgPath__Historical_other}</p>
-                    <h3>Latitude</h3>
-                    <p>{props.shipwreck.Latitude}</p>
-                    <h3>Length</h3>
-                    <p>{props.shipwreck.Length}</p>
-                    <h3>Longitude</h3>
-                    <p>{props.shipwreck.Longitude}</p>
-                    <h3>Origin\_Port</h3>
-                    <p>{props.shipwreck.Origin_Port}</p>
-                    <h3>Site\_Description</h3>
-                    <p>{props.shipwreck.Site_Description}</p>
-                    <h3>Vessel\_Type</h3>
-                    <p>{props.shipwreck.Vessel_Type}</p>
-                    <h3>Year\_Built</h3>
-                    <p>{props.shipwreck.Year_Built}</p>
-                    <h3>Year\_Sank</h3>
-                    <p>{props.shipwreck.Year_Sank}</p>
+                    <div>
+                        <strong>id:</strong> <span>{props.shipwreck.id}</span>
+                    </div>
+                    <div>
+                        <strong>Depth:</strong> <span>{props.shipwreck.Depth}</span>
+                    </div>
+                    <div>
+                        <strong>Destination_Port:</strong> <span>{props.shipwreck.Destination_Port}</span>
+                    </div>
+                    <div>
+                        <strong>Lake:</strong> <span>{props.shipwreck.Lake}</span>
+                    </div>
+                    <div>
+                        <strong>Length:</strong> <span>{props.shipwreck.Length}</span>
+                    </div>
+                    <div>
+                        <strong>Site_Description:</strong> <span>{props.shipwreck.Site_Description}</span>
+                    </div>
+                    <div>
+                        <strong>Vessel_Type:</strong> <span>{props.shipwreck.Vessel_Type}</span>
+                    </div>
+                    <div>
+                        <strong>Year_Built:</strong> <span>{props.shipwreck.Year_Built}</span>
+                    </div>
+                    <div>
+                        <strong>Year_Sank:</strong> <span>{props.shipwreck.Year_Sank}</span>
+                    </div>
+                    <div>
+                        <img src={props.shipwreck.imgPath} alt="shipwreck" style={{ width: "100%" }} />
+                    </div>
+                </div>
             </div>
         )
 }
