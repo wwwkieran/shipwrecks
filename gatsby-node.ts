45cebc8734eca2b2e2fe7815ec3041299709a7e3
Kieran Parikh <kieran@parikh.ca>
2024-10-16 15:09:36 -0400

    remove ship pages

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,27 +1,27 @@
-import type { GatsbyNode } from "gatsby"
-
-const path = require('path')
-const shipTemplate = path.resolve('./src/pages/ship.tsx')
-
-
-export const createPages: GatsbyNode["createPages"] = async ({ actions, graphql }) => {
-    const { data } = await graphql(`
-    query shipData {
-      allDataCsv {
-        nodes {
-            id
-        }
-      }
-    }
-  `)
-    // @ts-ignore
-    data.allDataCsv.nodes.forEach((node, index) => {
-        actions.createPage({
-            path: 'ships/' + node.id,
-            component: `${shipTemplate}`,
-            context: {
-                id: node.id,
-            }
-        })
-    })
-}
+// import type { GatsbyNode } from "gatsby"
+//
+// const path = require('path')
+// const shipTemplate = path.resolve('./src/pages/ship.tsx')
+//
+//
+// export const createPages: GatsbyNode["createPages"] = async ({ actions, graphql }) => {
+//     const { data } = await graphql(`
+//     query shipData {
+//       allDataCsv {
+//         nodes {
+//             id
+//         }
+//       }
+//     }
+//   `)
+//     // @ts-ignore
+//     data.allDataCsv.nodes.forEach((node, index) => {
+//         actions.createPage({
+//             path: 'ships/' + node.id,
+//             component: `${shipTemplate}`,
+//             context: {
+//                 id: node.id,
+//             }
+//         })
+//     })
+// }
